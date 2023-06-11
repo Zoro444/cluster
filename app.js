76d2264a1eaa1a980b9d05b320a43c474e613d7e
Zoro444 <zoro.simonyan1995@gmail.com>
2023-06-11 22:47:46 +0400

    add csv parser functionality

--- app.js
+++ app.js
@@ -1,3 +1,100 @@
-import fs from "fs";
-import cluster from "cluster";
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+const cluster = require('cluster');
 
+const csvDirectory = process.argv[2];
+
+if (!csvDirectory) {
+  console.error('Select csv directory path!');
+  process.exit();
+}
+
+if (cluster.isPrimary) {
+  const numCPUs = require('os').cpus().length;
+  let csvFiles = [];
+  let parsedCsv = [];
+
+  fs.promises.readdir(csvDirectory)
+    .then((data) => {      
+       csvFiles = data.filter((csvFile) => path.extname(csvFile) === '.csv');
+       for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+       } 
+       fs.promises.mkdir(path.join(csvDirectory, 'converted'))
+        .then((data) => {
+
+        })
+        .catch((err) => {
+            
+        });   
+    })
+    .catch((err) => {
+        console.log('No such directory!');
+    });
+
+  let fileIndex = -1;
+
+  cluster.on('online', worker => {
+    if (csvFiles.length - 1 > fileIndex) {
+        fileIndex++;
+        worker.send({ filePath: path.join(csvDirectory, csvFiles[fileIndex]), fileName: csvFiles[fileIndex] });     
+    }
+    else {
+        worker.send({ end: 'end' });
+    }
+
+    worker.on('message', message => {
+        parsedCsv.push(message)
+      });
+  });
+
+  cluster.on('exit', (data) => {
+    if (Object.keys(cluster.workers).length === 0) {
+        console.log(parsedCsv);
+    }
+    if (csvFiles.length - 1 > fileIndex) {
+        cluster.fork();
+    }
+  });
+
+} 
+else {
+  process.on('message', message => {  
+    if(message.end) {
+      process.exit();
+    }
+
+    let start;
+
+    const csvData = fs.createReadStream(path.join(path.resolve(), message.filePath), "utf-8");
+    
+    const csvDataWriteJson = fs.createWriteStream(
+      path.join(
+        path.resolve(), csvDirectory, 'converted', 
+        `${path.basename(message.fileName, path.extname(message.fileName))}.json`
+      ), "utf-8"
+    );
+
+    csvData.on('open', (data) => {
+      start = new Date();
+    })
+
+    csvData.on('data', (data) => {
+      start = new Date();
+    })
+
+    csvData.pipe(csvDataWriteJson);
+
+    csvData.on('close', (data) => {
+      console.log(message.filePath, new Date() - start);  
+      let parsingInfo = {
+        fileName: message.filePath,
+        duration: new Date() - start,
+      }   
+      process.send(parsingInfo);
+
+      process.exit();
+    })    
+  });
+}
